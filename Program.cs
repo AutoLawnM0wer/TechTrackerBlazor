08d007f8d71a8f7ec3580ebceb23789aa1437566
AutoLawnM0wer <143781463+AutoLawnM0wer@users.noreply.github.com>
2026-04-17 16:07:17 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+using TechTrackerBlazor.Components;
+using TechTrackerBlazor.Settings;
+
+namespace TechTrackerBlazor
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
+            builder.Services.Configure<MongoDbSettings>(
+                builder.Configuration.GetSection("MongoDbSettings"));
+
+            var app = builder.Build();
+
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseAntiforgery();
+
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            app.Run();
+        }
+    }
+}
