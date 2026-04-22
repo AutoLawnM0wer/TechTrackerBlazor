1936cf187bbb54817adb075e53ea65ecc4a47961
Vanay Rowell <vanay.rowelllol@gmail.com>
2026-04-21 23:00:46 -0400

    Customer.cs

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,29 +1,50 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace TechTrackerBlazor.Models
 {
     public class Customer
     {
-        public string Id { get; set; } = Guid.NewGuid().ToString("N");
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
 
         [Required]
         [MaxLength(40)]
+        [BsonElement("FirstName")]
         public string FirstName { get; set; } = string.Empty;
 
         [Required]
         [MaxLength(40)]
+        [BsonElement("LastName")]
         public string LastName { get; set; } = string.Empty;
 
         [EmailAddress]
+        [BsonElement("Email")]
         public string? Email { get; set; }
 
         [Required]
         [Phone]
+        [BsonElement("Phone")]
         public string Phone { get; set; } = string.Empty;
+
+        [BsonElement("CreationDate")]
         public DateTime CreationDate { get; set; } = DateTime.Today;
+
+        [BsonIgnore]
+        public string SearchFirstName { get; set; } = string.Empty;
+        
+        [BsonIgnore]
+        public string SearchLastName { get; set; } = string.Empty;
+        
+        [BsonIgnore]
+        public string SearchEmail { get; set; } = string.Empty;
+        
+        [BsonIgnore]
+        public string SearchPhone { get; set; } = string.Empty;
 
         ///indexes to-be-implemented: FirstName, LastName, Email, Phone  aislopfilledthistext
         ///NOT RESTRICTED TO THESE!!! ADD/CHANGE IF WE NEED MORE OR BETTER ONES
-
     }
 }
