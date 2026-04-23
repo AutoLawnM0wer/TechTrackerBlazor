8a6957c07d5bf46a5e9931bd111c77dcd70cf642
Vanay Rowell <vanay.rowelllol@gmail.com>
2026-04-22 21:45:09 -0400

    TransactionRecord.cs

--- Models/TransactionRecord.cs
+++ Models/TransactionRecord.cs
@@ -1,6 +1,28 @@
-﻿namespace TechTrackerBlazor.Models
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace TechTrackerBlazor.Models
 {
     public class TransactionRecord
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("OrderId")]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? OrderId { get; set; }
+
+        [BsonElement("Cost")]
+        public decimal Cost { get; set; }
+
+        [BsonElement("PaymentMethod")]
+        public string? PaymentMethod { get; set; }
+
+        [BsonElement("Date")]
+        public DateTime Date { get; set; }
+
+        [BsonElement("Completed")] 
+        public bool Completed { get; set; }
     }
 }
