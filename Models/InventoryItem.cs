eb73c8e8d971ce00a4c229aaa98887f916444328
Vanay Rowell <vanay.rowelllol@gmail.com>
2026-04-30 12:17:09 -0400

    InventoryItem.cs

--- Models/InventoryItem.cs
+++ Models/InventoryItem.cs
@@ -1,6 +1,25 @@
-﻿namespace TechTrackerBlazor.Models
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace TechTrackerBlazor.Models
 {
     public class InventoryItem
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        public string PartName { get; set; } = "";
+        public string? PartLocation { get; set; }
+        
+        public int Stock { get; set; }
+        public int LowStockThreshold { get; set; }
+        public decimal Cost { get; set; }
+
+        [BsonElement("CompatabilityTag")]
+        public string? CompatibilityTag { get; set; }
+
+        [BsonElement("supplier")]
+        public string? Supplier { get; set; }
     }
 }
